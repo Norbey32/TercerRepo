8ed7ea3576a484b708e74d043eb2bed7041e6bbc
Norbey <norbeyd_gutierrez@soy.sena.edu.co>
2025-10-03 13:22:54 -0500

    primer lanzamiento

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="TercerRepo",                           # Nombre del paquete
+    version="0.1.0",                                # Versión inicial
+    packages=find_packages(),                       # Paquetes a incluir
+    description="Un paquete pip simple de saludo",  # Breve descripción
+    author="Norbey Gutierrez",                         # Tu nombre
+    author_email="norbey97gutierrez@gmail.com",                 # Tu correo electrónico
+    url="https://github.com/Norbey32/TercerRepo",     # URL del proyecto
+)
